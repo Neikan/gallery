65d30b3b7f84ec4a06e3ab3723064a45f145973b
Neikan <sladkov.e.m@outlook.com>
2023-04-26 22:48:42 +0300

    feat(data): fix RepositoryPhoto

--- lib/data/repositories/repository_photo/repository_photo.dart
+++ lib/data/repositories/repository_photo/repository_photo.dart
@@ -4,5 +4,5 @@ import 'package:gallery_app/data/models/api_photo/api_photo.dart';
 abstract class RepositoryPhoto {
   const RepositoryPhoto();
 
-  Future<ApiPhoto?> getData(String id);
+  Future<ApiPhoto?> getData(int id);
 }
--- lib/data/repositories/repository_photo/repository_photo_imp.dart
+++ lib/data/repositories/repository_photo/repository_photo_imp.dart
@@ -1,4 +1,5 @@
 // Project imports:
+import 'package:gallery_app/data/consts/urls.dart';
 import 'package:gallery_app/data/models/api_photo/api_photo.dart';
 import 'package:gallery_app/data/repositories/repository_photo/repository_photo.dart';
 import 'package:gallery_app/data/services/service_http.dart';
@@ -7,10 +8,10 @@ class RepositoryPhotoImp extends RepositoryPhoto {
   const RepositoryPhotoImp();
 
   @override
-  Future<ApiPhoto?> getData(String id) async {
+  Future<ApiPhoto?> getData(int id) async {
     ApiPhoto? photo;
 
-    final response = await ServiceHttp.instance.get(id);
+    final response = await ServiceHttp.instance.get('$urlPhotos/$id');
 
     if (response.statusCode == 200) {
       photo = ApiPhoto.fromJson(response.data);
